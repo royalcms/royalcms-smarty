249e8f08dc0ff35ef1b153cc28c8b259b0a8bad9
royalwang <master@it86.cc>
2016-09-04 20:14:23 +0800

    测试入口文件更新

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -6,14 +6,7 @@
 /*
  * Smarty PHPUnit Bootstrap
  */
-// Locate SmartyBC class and load it
-if (is_file(__DIR__ . '/../smarty/libs/SmartyBC.class.php')) {
-    require_once __DIR__ . '/../smarty/libs/SmartyBC.class.php';
-} elseif (is_file(__DIR__ . '/../libs/SmartyBC.class.php')) {
-    require_once __DIR__ . '/../libs/SmartyBC.class.php';
-} else {
-    throw new Exception('can not locate Smarty distribution');
-}
+// Locate Smarty class and load it
 if (!defined('SMARTY_COMPOSER_INSTALL')) {
     foreach (array(__DIR__ . '/../../autoload.php', __DIR__ . '/../vendor/autoload.php', __DIR__ . '/vendor/autoload.php') as $file) {
         if (file_exists($file)) {
